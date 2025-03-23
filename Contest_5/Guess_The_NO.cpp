4b9f9be1e6adf22d39407775931b309fd6a9dc72
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-03-23 23:16:15 +0600

    Guess the NO

---
+++ Contest_5/Guess_The_NO.cpp
@@ -0,0 +1,18 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    if(t == 3)
+        cout << "YES";
+    else if(t == 4)
+        cout << "YES";
+    else if (t >= 6)
+        cout << "YES";
+    else
+        cout << "NO";
+    
+    return 0;
+}
