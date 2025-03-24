414a4b28248e97bac03c4ef94dcd01b8a66664b4
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-03-24 22:21:22 +0600

    string decoder

---
+++ Contest_6/String_Decoder.cpp
@@ -0,0 +1,24 @@
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int t;
+//     cin >> t;
+//     string s;
+//     while(t--){
+//         cin >> s;
+//         int size = s.size();
+//         string ch;
+//         for(int i=0,j=1; j<size; i+=2,j+=2){
+//             int encript = s[j] - '0';
+//             ch.append(encript,s[i]);
+//         }
+//         cout << ch << endl;
+//     }
+
+//     return 0;
+// }
+
+
+
